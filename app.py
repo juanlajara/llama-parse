152a36989357f091ee8395445bcd757e65aa18ed
juanlajara <juanlajara001@gmail.com>
2024-06-05 21:21:01 -0500

    added instructions prompt to llama parser

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-document = LlamaParse(result_type="markdown").load_data("apple10K.pdf")
+""" document = LlamaParse(result_type="markdown").load_data("FastFoods2020.pdf")
 
 print(document[0].text[:1000])
 
-file_name = "apple_10k.md"
+file_name = "FastFoods2020.md"
 with open(file_name, 'w') as file:
-    file.write(document[0].text)
+    file.write(document[0].text) """
+
+
+documents_with_instruction = LlamaParse(
+    result_type="markdown",
+    parsing_instructions=""" This is a Schedule of Property Values 
+    for a fast food account, I would like a summary of total insured value by state"""
+).load_data("FastFoods2020.pdf")
+
+file_name = "FastFoods2020.md"
+with open(file_name, 'w') as file:
+    file.write(documents_with_instruction[0].text)
